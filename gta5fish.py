374a0a5e5e6132d259cc6c89b036c5a4a6ae720b
zxchurka <86516807+FinKaHn@users.noreply.github.com>
2022-10-12 15:21:03 +0500

    Add files via upload
    калькулятор рыбы для GTA 5RP

---
+++ gta5fish.py
@@ -0,0 +1,44 @@
+# zxchurka? :/
+
+
+losos = int(input("Лосось: "))
+
+print(losos*118)
+
+osetr = int(input("Осетр: "))
+
+print(osetr*116)
+
+sterld = int(input("Стерлядь: "))
+
+print(sterld*47)
+
+amyr = int(input("Чер.Амур: "))
+
+print(amyr*231)
+
+skat = int(input("Скат: "))
+
+print(skat*263)
+
+tynec = int(input("Тунец: "))
+
+print(tynec*735)
+
+malma = int(input("Мальма: "))
+
+print(malma*1103)
+
+fygy = int(input("Фугу: "))
+
+print(fygy*2625)
+
+vsego = print("Всего рыбы:")
+
+print(losos+osetr+sterld+amyr+skat+tynec+malma+fygy)
+
+print("Сумма: ")
+
+sum = losos*118 + osetr*116 + sterld*47 + amyr*231 + skat*263 + tynec*735 + malma*1103 + fygy*2625 - 2000
+
+print(sum)
